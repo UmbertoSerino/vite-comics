72a2975aa435bdffd84916e299b589abe5d0bc2e
UmbertoSerino <serumberto@gmail.com>
2023-12-14 16:20:16 +0100

    created App Vite

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: AppFooter,
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: AppMain,
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
